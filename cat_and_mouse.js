f487b6936a4f072103c00350497627fd53f34178
Muzammil Hussain <muzammilhussain98632@gmail.com>
2025-07-14 20:21:32 +0500

    HackerRank : cat_and_mouse.js solution

---
+++ cat_and_mouse.js
@@ -0,0 +1,12 @@
+function catAndMouse(x, y, z) {
+    let A = Math.abs(x - z);
+    let B = Math.abs(y - z);
+    if (A < B) {
+        return "Cat A";
+    } else if (B < A) {
+        return "Cat B";
+    } else {
+        return "Mouse C";
+    }
+}
+console.log(catAndMouse([[1,2,3],[1,3,2]]));
