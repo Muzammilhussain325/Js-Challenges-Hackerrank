da06556749c23b50ff122ce668bff984ebf08f8f
Muzammil Hussain <muzammilhussain98632@gmail.com>
2025-07-15 22:01:36 +0500

    HackerRank : counting_valleys.js solution

---
+++ counting_valleys.js
@@ -0,0 +1,18 @@
+function countingValleys(steps, path) {
+    let sea = 0;
+    let Count = 0;
+    let alt = 0;
+
+    for (let i = 0; i < steps; i++) {
+        if (path[i] == 'U') {
+            alt++;
+            if (alt == 0) {
+                Count++;
+            }
+        } else if (path[i] =='D') {
+            alt--;
+        }
+    }
+    return Count;
+}
+console.log(countingValleys(8, "UDDDUDUU"));
